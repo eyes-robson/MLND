0496763ef72e90d55ab55877be9270e201ee687b
isrobson <isrobson@email.unc.edu>
2017-09-04 05:47:11 +0000

    revert to default-learning

--- machine-learning/projects/smartcab/smartcab/agent.py
+++ machine-learning/projects/smartcab/smartcab/agent.py
@@ -39,9 +39,9 @@ class LearningAgent(Agent):
         # Update epsilon using a decay function of your choice
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
-	#self.epsilon -= 0.05
-	self.epsilon = self.epsilon * math.exp(-self.alpha/10) # exponential decay for epsilon
-	self.alpha += (1 - self.alpha)/100
+	self.epsilon -= 0.05
+	#self.epsilon = self.epsilon * math.exp(-self.alpha/10) # exponential decay for epsilon
+	#self.alpha += (1 - self.alpha)/100
 	if(testing == True):
 		self.epsilon = 0.0
 		self.alpha = 0.0
@@ -191,7 +191,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, learning = True, alpha = .1)
+    agent = env.create_agent(LearningAgent, learning = True)
     
     ##############
     # Follow the driving agent
@@ -206,14 +206,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.005, display=False, log_metrics = True, optimized=True)
+    sim = Simulator(env, update_delay=0.01, display=False, log_metrics = True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(tolerance = 0.02, n_test=30)
+    sim.run(n_test=20)
 
 
 if __name__ == '__main__':
